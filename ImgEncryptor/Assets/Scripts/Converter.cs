ec43957a1a4667d6862589e8d62796bf8c7fe7bf
slapota <arnost.spalensky@gmail.com>
2024-05-21 09:10:33 +0200

    done
    nechapu jak to muze fungovat ale zmenil jsem color threshold z 255 na 256 a workuje to

--- ImgEncryptor/Assets/Scripts/Converter.cs
+++ ImgEncryptor/Assets/Scripts/Converter.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using System.IO;
+using System.Runtime.InteropServices;
 
 public class Converter : MonoBehaviour
 {
     public Image img;
     public Canvas canvas;
     string path;
+    string imgName;
     public void Apply()
     {
         path = EditorUtility.OpenFilePanel("Load your img", "", "png");
@@ -18,7 +20,9 @@ public class Converter : MonoBehaviour
         temp.LoadImage(File.ReadAllBytes(path));
         img.sprite = Sprite.Create(temp, new Rect(0, 0, temp.width, temp.height), new Vector2(0, 0));
 
-        if(temp.width > temp.height)
+        imgName = path.Split('/')[path.Split('/').Length - 1].Split('.')[0];
+
+        if (temp.width > temp.height)
         {
             float factor = (canvas.pixelRect.width/2) / temp.width;
             img.rectTransform.sizeDelta = new Vector2(canvas.pixelRect.width/2, temp.height*factor);
@@ -36,7 +40,7 @@ public class Converter : MonoBehaviour
         byte[] temp;
         temp = img.sprite.texture.EncodeToPNG();
         string desPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);
-        File.WriteAllBytes(desPath + "/" + path.Split('/')[path.Split('/').Length - 1].Split('.')[0] + ".png", temp);
+        File.WriteAllBytes(desPath + "/" + imgName + "_converter.png", temp);
     }
     public void Encode()
     {
@@ -49,29 +53,47 @@ public class Converter : MonoBehaviour
             byte factorR = (byte)Random.Range(0, 255);
             byte factorG = (byte)Random.Range(0, 255);
             byte factorB = (byte)Random.Range(0, 255);
-            byte factor4 = (byte)Random.Range(0, 255);
-            pixels[i] = new Color32((byte)Loop(pixels[i].r + factorR), (byte)Loop(pixels[i].g + factorG), (byte)Loop(pixels[i].g + factorB), (byte)Loop(pixels[i].a+factor4));
-            factors[i] = new Color32(factorR, factorG, factorB, factor4);
+            //byte factorA = (byte)Random.Range(0, 255);
+            pixels[i] = new Color32((byte)Loop(pixels[i].r + factorR), (byte)Loop(pixels[i].g + factorG), (byte)Loop(pixels[i].b + factorB), pixels[i].a);
+            factors[i] = new Color32(factorR, factorG, factorB, 0);
         }
 
         temp.SetPixels32(pixels);
 
         img.sprite = Sprite.Create(temp, new Rect(0, 0, temp.width, temp.height), new Vector2(0, 0));
         temp.Apply();
+
+        string desPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+        File.WriteAllBytes(desPath + "/" + imgName + "_key_" + Random.Range(0, 9999999) + ".txt", MemoryMarshal.Cast<Color32, byte>(factors).ToArray());
     }
     public void Decode()
     {
-        
+        string keyPath = EditorUtility.OpenFilePanel("Load your key", "", "txt");
+        byte[] temp = File.ReadAllBytes(keyPath);
+        Color32[] data = MemoryMarshal.Cast<byte, Color32>(temp).ToArray();
+
+        Texture2D texture = img.sprite.texture;
+        Color32[] pixels = texture.GetPixels32();
+
+        for(int i = 0; i < pixels.Length; i++)
+        {
+            Color32 c = data[i];
+
+            pixels[i] = new Color32((byte)Loop(pixels[i].r - c.r), (byte)Loop(pixels[i].g - c.g), (byte)Loop(pixels[i].b - c.b), pixels[i].a);
+        }
+        texture.SetPixels32(pixels);
+        img.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
+        texture.Apply();
     }
     float Loop(float index)
     {
-        if (index > 255)
+        if (index > 256)
         {
-            return index - 255;
+            return index - 256;
         }
         if(index < 0)
         {
-            return index + 255;
+            return index + 256;
         }
         return index;
     }
